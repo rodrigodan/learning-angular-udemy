62c14ccf65cb656972e43a5b84a4f2e218dbd1ee
Goncalves Daniel <rgdaniel@indracompany.com>
2020-04-05 19:02:27 -0300

    #Understanding Observable - Subjects

--- Understanding Observable/src/app/app.component.ts
+++ Understanding Observable/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs'
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit,OnDestroy {
+  private activatedSub: Subscription;
+  userActivated = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate =>{
+      this.userActivated = didActivate;
+    })
+  }
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe()
+  }
 }
