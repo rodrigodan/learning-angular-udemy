5610514fa6c7b47f660f8d04a46832b24aecdca6
Goncalves Daniel <rgdaniel@indracompany.com>
2020-05-17 23:55:42 -0300

    #Handling Forms in Angular App: Reactive: Setting and Patching Values

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -29,10 +29,17 @@ export class AppComponent implements OnInit{
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna'
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby(){
