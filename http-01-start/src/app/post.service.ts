457e7f5b624fa0272ea93de3d9f106bd972867d9
Goncalves Daniel <rgdaniel@indracompany.com>
2020-05-24 03:10:18 -0300

    #Making Http Requests - Using the catchError Operator

--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { NotExpr } from '@angular/compiler';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -39,7 +38,12 @@ export class PostService{
             }
           }
           return postArray;
-        }));
+        }),
+        catchError(errorRes => {
+            //send analytic server
+            return throwError(errorRes);
+        })
+        );
     }
 
     deletePosts(){
