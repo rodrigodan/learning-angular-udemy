20fe3d9417630eeb562628e90ec166bedc155739
Goncalves Daniel <rgdaniel@indracompany.com>
2020-06-22 22:48:46 -0300

    #Authentication & RouteProtection in Angular - Reflecting the Auth State in the UI

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -14,7 +15,7 @@ export class AuthComponent {
 
 
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){}
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
@@ -37,6 +38,7 @@ export class AuthComponent {
       resData =>{
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(['/recipes']);
   
       },
       errorMessage => {
