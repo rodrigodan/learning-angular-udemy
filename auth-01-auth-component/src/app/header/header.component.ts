cad67fcd8ab04e93f2fb3c43bd0b731618fbcea7
Rodrigo Goncalves <rodrigogoncalvesdan@gmail.com>
2020-07-13 23:58:27 -0300

    Authentication & Route Protection in Angular: Adding Logout

--- auth-01-auth-component/src/app/header/header.component.ts
+++ auth-01-auth-component/src/app/header/header.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit, OnDestroy{
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService, private router: Router) {}
   private userSub: Subscription;
   isAuthenticated = false;
   ngOnInit(){
@@ -26,6 +27,11 @@ export class HeaderComponent implements OnInit, OnDestroy{
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth']);
   }
 
   ngOnDestroy(){
